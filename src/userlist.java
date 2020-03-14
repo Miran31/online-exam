fdc75000bf415eb4aaf667e11d7ccabb37b103c2
Jannat-Dhina <61325458+Jannat-Dhina@users.noreply.github.com>
2020-03-14 13:00:33 +0600

    Add files via upload

---
+++ src/userlist.java
@@ -0,0 +1,25 @@
+import dao.UserDao;
+import model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class userlist extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        UserDao userDao = new UserDao();
+        List<User> users = userDao.findAllUser();
+        System.out.println("# "+users.toString());
+
+    }
+}
